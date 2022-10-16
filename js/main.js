d7824f73654b66e3c0b6962bda4d76c21d33c72c
Diego BustamantediegoBustamante <diegi.b58@gmail.com>
2022-10-16 04:18:55 -0300

    entrega Js

--- js/main.js
+++ js/main.js
@@ -1,18 +1,23 @@
 let ticketPrice = 200
 let total = document.getElementById('total')
-const borrar = document.getElementById('borrar')
+
 const resumen = document.getElementById('resumen')
 
 
-borrar.addEventListener("click", function borrar() {
+
+
+
+function borrar() {
     total.innerHTML= ` Total a Pagar: $`
-})
+}
+
 
 
 function mostrar(categoria) {
     let cantidad = document.getElementById("cantidad").value
     if (cantidad < 1 || cantidad > 20 ) {
         document.getElementById("error").innerHTML = `<div>Cantidad  <span class="text-danger  fw-bold">Debe Ingresar un numero mayor a 0 y menor a 20</span></div>`
+        borrar()
     } else {
         document.getElementById("error").innerHTML = `Cantidad`
         if (categoria == 1) {
